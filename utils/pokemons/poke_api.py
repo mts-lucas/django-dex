8125a43c5c17a15eecc331ac216c55c3d60c5b26
Lucas Mateus <lmateus1067@outlook.com>
2023-01-30 09:55:01 -0300

    get details

--- utils/pokemons/poke_api.py
+++ utils/pokemons/poke_api.py
@@ -26,8 +26,8 @@ class Pokemon():
     def __init__(self):
         self.id = None
         self.name = None
-        self.type = None
         self.types = None
+        self.type = None
         self.height = None
         self.width = None
         self.photo = None
@@ -41,4 +41,33 @@ class Pokemon():
             statusNumber = status['base_stat']
             newStatsDict[statusName] = statusNumber
 
-        self.stats = newStatsDict
+        return newStatsDict
+
+    def getTypes(self, pokemonjson):
+        typeDict = pokemonjson['types']
+        typeList = []
+        for tipos in typeDict:
+            typeList.append(tipos['type']['name'])
+
+        return typeList
+
+    def getSprite(self, pokemonjson):
+        spritesDict = pokemonjson['sprites']['other']
+        pokemonSprite = spritesDict['official-artwork']['front_default']
+
+        return pokemonSprite
+
+    def getPokemonDetail(self, pokemonurl):
+
+        r = requests.get(pokemonurl)
+
+        pokemonData = r.json()
+
+        self.id = pokemonData['id']
+        self.name = pokemonData['name']
+        self.types = self.getTypes(pokemonData)
+        self.type = self.types[0]
+        self.height = pokemonData['height']
+        self.weight = pokemonData['weight']
+        self.photo = self.getSprite(pokemonData)
+        self.stats = self.getStats(pokemonData)
