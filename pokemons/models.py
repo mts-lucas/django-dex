29250c038a53e4c491445f690ae8ecc85aa63398
Lucas Mateus <lmateus1067@outlook.com>
2023-02-17 16:53:18 -0300

    criando primeiros modelos do banco

--- pokemons/models.py
+++ pokemons/models.py
@@ -3,10 +3,39 @@ from django.db import models
 # Create your models here.
 
 
+class PkmType(models.Model):
+    name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+
 class Pokemon(models.Model):
 
     number = models.IntegerField()
     name = models.CharField(max_length=65)
     picture = models.URLField()
+    #  physical attributes
     height = models.IntegerField()
     width = models.IntegerField()
+    #  status base
+    hp = models.IntegerField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    special_attack = models.IntegerField()
+    special_defense = models.IntegerField()
+    speed = models.IntegerField()
+    #  pokemon types
+    pkm_types = models.ForeignKey(
+        PkmType,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        default=None,)
+    color_type = models.CharField(max_length=35)
+
+    def __str__(self):
+
+        infos = str(self.number) + ' - ' + self.name
+
+        return infos
