7981e74ff9bf6329cb79682a013c6a4a080648f3
Lucas Mateus <lmateus1067@outlook.com>
2023-03-08 13:13:48 -0300

    estilizando

--- pokemons/urls.py
+++ pokemons/urls.py
@@ -6,5 +6,6 @@ app_name = 'pokemons'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('search', views.search_results, name="search_results"),
+    path('search/', views.search_results, name="search_results"),
+    path('type/<str:name>/', views.pkm_type, name="tipo")
 ]
