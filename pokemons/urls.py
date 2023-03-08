d163df37065a7b90df06246e75cc39e430b7337c
Lucas Mateus <lmateus1067@outlook.com>
2023-03-08 15:55:51 -0300

    criando url da ability

--- pokemons/urls.py
+++ pokemons/urls.py
@@ -7,5 +7,6 @@ app_name = 'pokemons'
 urlpatterns = [
     path('', views.home, name="home"),
     path('search/', views.search_results, name="search_results"),
-    path('type/<str:name>/', views.pkm_type, name="tipo")
+    path('type/<str:name>/', views.pkm_type, name="tipo"),
+    path('ability/<str:name>/', views.ability, name="ability"),
 ]
