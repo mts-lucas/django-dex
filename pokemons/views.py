22a4d3887d5aa455b0e366bc71c0d32d6ed64c6b
Lucas Mateus <lmateus1067@outlook.com>
2023-03-08 09:36:16 -0300

    criando view de tipos

--- pokemons/views.py
+++ pokemons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from .models import Pokemon
 
@@ -21,4 +21,30 @@ def search_results(request):
         results = Pokemon.objects.filter(name__icontains=query)
     return render(request, 'pokemons/pages/search-results.html', context={
         'results': results,
+    })
+
+
+def pkm_type(request, name):
+
+    pkmList = get_list_or_404(
+        Pokemon.objects.filter(
+            pkm_types__name__icontains=name
+        ).order_by('number'))
+
+    title = ''
+    for pokemon in pkmList:
+        for pkm_type in pokemon.pkm_types.all():
+            if pkm_type.name.lower() == name.lower():
+                title = pkm_type.name
+                break
+        if title:
+            break
+
+    if not title and pkmList:
+        title = "NotFound"
+
+    return render(request, 'pokemons/pages/tipos.html', context={
+
+        'pokemonItens': pkmList,
+        'tittle': title,
     })
